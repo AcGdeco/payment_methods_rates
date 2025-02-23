fce5d886990882a2231fcbd8bc417983cf22b7e4
AcGdeco <andrecristiangoncalves@hotmail.com>
2025-02-23 11:15:22 -0300

    :hammer: Uninstall

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -2,25 +2,30 @@
 
 namespace Deco\Rates\Setup;
 
+use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
-use Magento\Framework\Setup\SchemaSetupInterface;
-use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use \Magento\Catalog\Model\Product;
 
-class Uninstall implements UninstallInterface
+class InstallData implements InstallDataInterface
 {
-    private $eavSetupFactory;
+private $eavSetupFactory;
 
-    public function __construct(EavSetupFactory $eavSetupFactory)
-    {
-        $this->eavSetupFactory = $eavSetupFactory;
-    }
+public function __construct(EavSetupFactory $eavSetupFactory) 
+{
+ $this->eavSetupFactory = $eavSetupFactory;
+}
 
-    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
-    {
-        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-        $eavSetup->removeAttribute(
-            \Magento\Catalog\Model\Product::ENTITY,
-            'taxa_produto');
-    }
+ public function install(ModuleDataSetupInterface $setup, ModuleContextInterface 
+  $context)
+   {
+    $setup->startSetup();
+
+   $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+   $eavSetup->removeAttribute(Product::ENTITY, 'taxa_produto');
+
+  $setup->endSetup();
+  }
 }
