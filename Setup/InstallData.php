71eb3b6c4dd91dc69c628dc148a4c8173dd3a6a5
AcGdeco <andrecristiangoncalves@hotmail.com>
2025-02-23 10:35:55 -0300

    :heavy_plus_sign: Attribute

---
+++ Setup/InstallData.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Deco\Rates\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'taxa_produto', // Código do atributo
+            [
+                'type' => 'decimal', // Tipo de dado (varchar, int, text, etc.)
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Taxa Produto', // Rótulo do atributo
+                'input' => 'price', // Tipo de input (text, select, textarea, etc.)
+                'class' => '',
+                'source' => '',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => true,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => 'simple,configurable,virtual,bundle,downloadable' // Tipos de produto aplicáveis
+            ]
+        );
+    }
+}
---
+++ Setup/Uninstall.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Deco\Rates\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+class Uninstall implements UninstallInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->removeAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'taxa_produto');
+    }
+}
