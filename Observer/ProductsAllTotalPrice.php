f7cc0294559f24023c12a4c7f9879f7dd74e5dee
AcGdeco <andrecristiangoncalves@hotmail.com>
2025-03-14 19:35:37 -0300

    :heavy_plus_sign: Observer All Products Total Price

---
+++ Observer/ProductsAllTotalPrice.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Deco\Rates\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer as EventObserver;
+use Deco\Rates\Block\UpdateTotalPrice;
+
+class ConfigObserver implements ObserverInterface
+{
+    /**
+     * @var UpdateTotalPrice
+     */
+    protected $updateTotalPrice;
+
+    /**
+     * @param UpdateTotalPrice $updateTotalPrice
+     */
+    public function __construct(
+        UpdateTotalPrice $updateTotalPrice
+    ) {
+        $this->updateTotalPrice = $updateTotalPrice;
+    }
+
+    public function execute(EventObserver $observer)
+    {
+        $this->updateTotalPrice->addTotalPriceProductsValues();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # payment_methods_rates
+A configuração do módulo habilitado padrão deve ser deixada como desabilitado pois scripts são rodados após a habilitação do módulo
