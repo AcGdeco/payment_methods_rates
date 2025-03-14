bb181f152e199963a0c671ba1e82627ed304774f
AcGdeco <andrecristiangoncalves@hotmail.com>
2025-03-14 18:14:04 -0300

    :hammer: Fixing

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -5,15 +5,8 @@ namespace Deco\Rates\Setup;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
-use Deco\Rates\Block\UpdateTotalPrice;
 
 class UpgradeSchema implements UpgradeSchemaInterface {
-
-    public function __construct(
-        UpdateTotalPrice $updateTotalPrice
-    ) {
-        $this->updateTotalPrice = $updateTotalPrice;
-    }
 
     /**
      * {@inheritdoc}
