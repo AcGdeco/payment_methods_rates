6440c68cc021cdf21bd7e9745c1b4d1ee2411c79
AcGdeco <andrecristiangoncalves@hotmail.com>
2025-03-14 14:37:35 -0300

    :hammer: Disabled Attribute

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -102,6 +102,7 @@ class InstallData implements InstallDataInterface
                 'used_in_product_listing' => true,
                 'used_for_sort_by' => true,
                 'unique' => false,
+                'disable' => true,
                 'apply_to' => 'simple,configurable,virtual,bundle,downloadable'
             ]
         );
